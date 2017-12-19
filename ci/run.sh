896c173f10b7dfc1eb359e9c4fbb47fc4ed050a5
Brian Olsen <brian@maven-group.org>
2017-12-19 03:14:00 +0100

    Install protoc in docker image

--- ci/run.sh
+++ ci/run.sh
@@ -9,3 +9,4 @@ bundle exec jekyll build
 cd ..
 curl -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
 tar xvf GeoLite2-City.tar.gz --strip-components=1 GeoLite2-City_*/GeoLite2-City.mmdb
+ci/local
